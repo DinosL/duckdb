d2b50fc0f706db1880d41d5748ee43ac53510806
DinosL <konstantinos@duckdblabs.com>
2025-11-10 14:14:56 +0100

    tidy fix

--- src/optimizer/window_rewriter.cpp
+++ src/optimizer/window_rewriter.cpp
@@ -3,6 +3,9 @@
 #include "duckdb/planner/operator/logical_window.hpp"
 #include "duckdb/planner/operator/logical_projection.hpp"
 #include "duckdb/planner/expression/bound_columnref_expression.hpp"
+#include "duckdb/planner/operator/logical_get.hpp"
+#include "duckdb/planner/expression/bound_window_expression.hpp"
+#include "duckdb/optimizer/optimizer.hpp"
 
 namespace duckdb {
 

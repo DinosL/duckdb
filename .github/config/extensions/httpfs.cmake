cb97a2f52f2b0539a20fa07632cbde31e9a588e8
DinosL <konstantinos@duckdblabs.com>
2026-01-19 09:39:05 +0100

    bump httpfs

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -1,7 +1,7 @@
 duckdb_extension_load(httpfs
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG 6c187d86edde066adb2c51a411f3b6020da79e12
+    GIT_TAG b914a304e17e6fe57240fbbd8e7feaff09d1f24e
     APPLY_PATCHES
     INCLUDE_DIR src/include
 )

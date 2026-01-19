6efa6fb5e54830b27848f88e035a82fde6e70cdf
DinosL <konstantinos@duckdblabs.com>
2026-01-19 10:22:44 +0100

    remove apply patches

--- .github/config/extensions/httpfs.cmake
+++ .github/config/extensions/httpfs.cmake
@@ -2,6 +2,5 @@ duckdb_extension_load(httpfs
     LOAD_TESTS
     GIT_URL https://github.com/duckdb/duckdb-httpfs
     GIT_TAG b914a304e17e6fe57240fbbd8e7feaff09d1f24e
-    APPLY_PATCHES
     INCLUDE_DIR src/include
 )
